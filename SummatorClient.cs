c0e271eb2a9566edb350605de7a9e58d55e1b0d1
Semyon Kononchuk <ksemyonv@gmail.com>
2015-04-30 20:11:23 +0600

    First commit - Summator

---
+++ SummatorClient.cs
@@ -0,0 +1,97 @@
+﻿// Client
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				//Создаем правильную строку с тремя разными числами ("правильная строка" означает, что в ней встреяаются только пробелы и цифры) 
+				bool ifMessageIncorrect = true;
+				string message = "";
+				while (ifMessageIncorrect)
+				{
+					Console.Clear();
+					Console.WriteLine("Введите три разных числа, через пробел. Пример:\"1 2 3\": \n");
+					message = Console.ReadLine();
+
+					bool ifLettersWereInMessage = false;
+					for (int i = 0; i < message.Length; i++)
+						if ((message[i] < '0' || message[i] > '9') && message[i] != ' ')
+						{
+							Console.WriteLine("\nОбнаружены буквы! Пожалуйста, вводите только цифры и пробелы между ними.\nНажмите Enter для повторного ввода...");
+							ifLettersWereInMessage = true;
+							break;
+						}
+					if (ifLettersWereInMessage)
+					{
+						Console.ReadLine();
+						continue;
+					}
+					string[] test = message.Split(' ');
+					if (test.Length != 3)
+					{
+						Console.WriteLine("\nПожалуйста, введите только три числа!\nНажмите Enter для повторного ввода...");
+						Console.ReadLine();
+						continue;
+					}
+					else
+						if (test[0].ToString() == test[1].ToString() || test[0].ToString() == test[2].ToString() || test[1].ToString() == test[2].ToString())
+						{
+							Console.WriteLine("\nПожалуйста, вводите разные числа!\nНажмите Enter для повторного ввода...");
+							Console.ReadLine();
+						}
+						else ifMessageIncorrect = false;
+				}
+ 
+				// Буфер для входящих данных
+				byte[] buffer = new byte[1024];
+
+				// Соединяемся с сервером
+
+				// Устанавливаем удаленную точку для сокета
+				IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+				IPAddress ipAddr = ipHost.AddressList[0];
+				IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+				Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+				// Соединяем сокет с удаленной точкой
+				sender.Connect(ipEndPoint);
+				Console.WriteLine("\nСоединение с сервером...");
+				byte[] msg = Encoding.UTF8.GetBytes(message);
+
+				// Отправляем данные через сокет
+				int bytesSent = sender.Send(msg);
+
+				// Получаем ответ от сервера
+				int bytesRec = sender.Receive(buffer);
+
+				Console.WriteLine("Ответ от сервера: {0}\n\n", Encoding.UTF8.GetString(buffer, 0, bytesRec));
+
+				// Разрываем соединение с сервером
+				sender.Shutdown(SocketShutdown.Both);
+				sender.Close();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+			finally
+			{
+				Console.ReadLine();
+			}
+		}
+
+		static void SendMessageFromSocket(int port)
+		{
+			
+		}
+	}
+}
---
+++ SummatorServer.cs
@@ -0,0 +1,68 @@
+﻿// Server
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketServer
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			// Устанавливаем для сокета локальную конечную точку
+			IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+			IPAddress ipAddr = ipHost.AddressList[0];
+			IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+			// Создаем сокет Tcp/Ip
+			Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+			// Назначаем сокет локальной конечной точке и слушаем входящие сокеты
+			try
+			{
+				sListener.Bind(ipEndPoint);
+				sListener.Listen(0);
+
+				// Начинаем слушать соединения
+				while (true)
+				{
+					Console.WriteLine("Ожидаем соединения с клиентом...\n");
+
+					// Программа приостанавливается, ожидая входящее соединение
+					Socket client = sListener.Accept();
+					string inputMessage = null;
+
+					// Мы дождались клиента, пытающегося с нами соединиться, и получаем от него сообщение с тремя числами
+					byte[] bytes = new byte[1024];
+					int bytesRec = client.Receive(bytes);
+					
+					inputMessage += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+					//Разбиваем строку с тремя числаи на отдельные числа. Затем суммируем их.
+					string[] numbers = inputMessage.Split(' ');
+					int summ = Convert.ToInt32(numbers[0]) + Convert.ToInt32(numbers[1]) + Convert.ToInt32(numbers[2]);
+
+					Console.Write(String.Format("Пришли данные от клиента. Cумма: {0} + {1} + {2} = {3}\n\n",numbers[0], numbers[1], numbers[2], summ));
+
+					// Отправляем полученный результат клиенту
+					string reply = String.Format("Сумма отправленных Вами чисел равна {0}", summ); 
+					byte[] msg = Encoding.UTF8.GetBytes(reply);
+					client.Send(msg);
+					
+					//Разрываем соединение с текущим клиентом
+					client.Shutdown(SocketShutdown.Both);
+					client.Close();
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+			finally
+			{
+				Console.ReadLine();
+			}
+		}
+	}
+}
